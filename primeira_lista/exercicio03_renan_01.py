49818729967b2749c615822c4018ec3bfc89a87e
Renan Martins Pimentel <renan.pimentel@gmail.com>
2013-09-26 14:21:31 +0000

    terceiro exercicio do primeiro modulo

---
+++ primeira_lista/exercicio03_renan_01.py
@@ -0,0 +1,12 @@
+dias = input("Digite os dias: ");
+horas = input("Digite as horas: ");
+minutos = input("Digite os minutos: ");
+segundos = input("Digite os segundos: ");
+
+segundosMinuto = 60;
+segundosHoras = 60 * segundosMinuto;
+segundosDia = 24 * segundosHoras;
+
+totalSegundo = (dias*segundosDia) + (horas*segundosHoras) + (minutos*segundosMinuto) + segundos;
+
+print totalSegundo, 'segundos';
